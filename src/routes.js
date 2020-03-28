908a003bc6c95c07f14529c608557bd5bfdb4485
Benicio <benicio.daniel@gmail.com>
2020-03-28 15:27:21 -0300

    Implementa Validacao em Create de Ong e Get Profile

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,38 @@ const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const routes = express.Router(); 
 
 routes.post('/session', SessionController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
 
-routes.get('/profile', ProfileController.index)
+
+/**
+ * Query
+ * Route
+ * Body
+ */
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}),OngController.create)
+
+/**
+ * Pra Authorization no Header
+ */
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown()
+}) , ProfileController.index)
 
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
